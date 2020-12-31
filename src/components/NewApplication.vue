a85b38afb933f651308eee318e27e2b5a1e08f4a
Linus Dehner <ldehner@student.tgm.ac.at>
2020-12-31 16:00:39 +0100

    designed new application

--- src/components/NewApplication.vue
+++ src/components/NewApplication.vue
@@ -2,16 +2,18 @@
   <b-container fluid>
     <b-row id="new-application-row" align-h="center">
       <b-col cols="12">
-        <b-container>
-          <b-row align-h="center">
+        <b-container fluid>
+          <b-row align-v="center" align-h="center">
             <b-col cols="12" md="6">
               <h1 id="new-application-heading">Neuen Antrag erstellen</h1>
             </b-col>
-            <div class="col-12 col-md-6"></div>
+            <div class="col-12 col-md-6" >
+              <button type="button" class="btn btn-outline-primary float-right" v-on:click="index()">Startseite</button>
+            </div>
           </b-row>
           <b-row>
             <b-col cols="12">
-              <b-breadcrumb :items="items"></b-breadcrumb>
+              <b-breadcrumb style="background-color:white" :items="items"></b-breadcrumb>
             </b-col>
           </b-row>
           <b-row id="na-srow" align-h="center" align-v="center">
@@ -23,14 +25,14 @@
                   v-on:click="school()"
                 >
                   <b-col cols="12">
-                    <h2 id="na-elem-h">Schulveranstaltung</h2>
+                    <h2 class="na-elem-h">Schulveranstaltung</h2>
                   </b-col>
                 </b-row>
               </b-container>
               <b-container id="na-elem-2" class="na-elem shadow-xl">
-                <b-row class="na-elem-sr" align-v="center" v-on:click="other()">
+                <b-row class="na-elem-sr" align-h="center" align-v="center" v-on:click="other()">
                   <b-col cols="12">
-                    <h2>Fortbildung, etc.</h2>
+                    <h2 class="na-elem-h">Fortbildung, etc.</h2>
                   </b-col>
                 </b-row>
               </b-container>
@@ -69,21 +71,26 @@ export default {
       if (this.checkClick()) {
         this.changeComponent("Others");
       }
+    },
+    index() {
+      if (this.checkClick()) {
+        this.changeComponent("Index");
+      }
     }
   },
   data() {
     return {
       items: [
+        //{
+        //  text: "Admin",
+        //  href: "#"
+        //},
+        //{
+        //  text: "Manage",
+        //  href: "#"
+        //},
         {
-          text: "Admin",
-          href: "#"
-        },
-        {
-          text: "Manage",
-          href: "#"
-        },
-        {
-          text: "Library",
+          text: "Antrag Übersicht",
           active: true
         }
       ]
--- src/components/new/School.vue
+++ src/components/new/School.vue
@@ -1,3 +1,100 @@
 <template>
-  <div></div>
+  <b-container fluid>
+    <b-row id="new-application-row" align-h="center">
+      <b-col cols="12">
+        <b-container fluid>
+          <b-row align-v="center" align-h="center">
+            <b-col cols="12" md="6">
+              <h1 id="new-application-heading">Neuen Antrag erstellen</h1>
+            </b-col>
+            <div class="col-12 col-md-6" >
+              <button type="button" class="btn btn-outline-primary float-right" v-on:click="index()">Startseite</button>
+            </div>
+          </b-row>
+          <b-row>
+            <b-col cols="12">
+              <b-breadcrumb style="background-color:white" :items="items"></b-breadcrumb>
+            </b-col>
+          </b-row>
+          <b-row id="na-srow" align-h="center" align-v="center">
+            <b-col cols="12" md="6" lg="4">
+              <b-container class="na-elem shadow-xl">
+                <b-row
+                  class="na-elem-sr"
+                  align-v="center"
+                  v-on:click="school()"
+                >
+                  <b-col cols="12">
+                    <h2 class="na-elem-h">Schulveranstaltung</h2>
+                  </b-col>
+                </b-row>
+              </b-container>
+              <b-container id="na-elem-2" class="na-elem shadow-xl">
+                <b-row class="na-elem-sr" align-h="center" align-v="center" v-on:click="other()">
+                  <b-col cols="12">
+                    <h2 class="na-elem-h">Fortbildung, etc.</h2>
+                  </b-col>
+                </b-row>
+              </b-container>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+<script>
+export default {
+  name: "NewApplication",
+  methods: {
+    changeComponent(component, back = true, application = null) {
+      this.$emit("change-component", component, back, application);
+    },
+    checkClick() {
+      if (
+        window
+          .getSelection()
+          .toString()
+          .trim() === ""
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    school() {
+      if (this.checkClick()) {
+        this.changeComponent("School");
+      }
+    },
+    other() {
+      if (this.checkClick()) {
+        this.changeComponent("Others");
+      }
+    },
+    index() {
+      if (this.checkClick()) {
+        this.changeComponent("Index");
+      }
+    }
+  },
+  data() {
+    return {
+      items: [
+        //{
+        //  text: "Admin",
+        //  href: "#"
+        //},
+        {
+          text: "Antrag Übersicht",
+          href: "#"
+        },
+        {
+          text: "Schulveranstaltung - Allg. Infos",
+          active: true
+        }
+      ]
+    };
+  }
+};
+</script>
