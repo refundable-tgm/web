310e5be136028213caa4d16e6769e4b973f00696
Ryan <43230399+rfoster93@users.noreply.github.com>
2020-12-31 15:59:25 +0100

    added Icon support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
