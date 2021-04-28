e7d701b28d797b09ed78ce665ecbd474b56b7307
Ryan <43230399+rfoster93@users.noreply.github.com>
2021-04-28 08:27:14 +0200

    fixed other openPDF functions

--- src/components/admin/AdminDashboard.vue
+++ src/components/admin/AdminDashboard.vue
@@ -1089,7 +1089,7 @@ export default {
       );
       pdfWindow.document.write(
         "<body><embed width='100%' height='100%' src='data:application/pdf;base64, " +
-          encodeURI(pdf) +
+          encodeURI(pdf.pdf) +
           "#toolbar=0&navpanes=0&scrollbar=0'></embed></body></html>"
       );
     },
