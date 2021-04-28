91e25c701c0f71959ff942ab79f541117d049378
Ryan <43230399+rfoster93@users.noreply.github.com>
2021-04-28 12:06:41 +0200

    fixing pdfViewer

--- src/components/admin/ApplicationAdminView.vue
+++ src/components/admin/ApplicationAdminView.vue
@@ -524,7 +524,7 @@ export default {
           "</title><style>body{margin: 0px;padding:0px;}iframe{border-width: 0px;}</style></head>"
       );
       pdfWindow.document.write(
-        "<body><embed width='100%' height='100%' src='data:application/pdf;base64, " +
+        "<body><embed width='100%' height='99%' src='data:application/pdf;base64, " +
           encodeURI(pdf.pdf) +
           "#toolbar=0&navpanes=0&scrollbar=0'></embed></body></html>"
       );
