227793e71ea3044949cc34d550c834fcb140a040
Ryan <43230399+rfoster93@users.noreply.github.com>
2021-04-28 14:07:20 +0200

    added logs to allApplication

--- src/components/AllApplication.vue
+++ src/components/AllApplication.vue
@@ -368,9 +368,11 @@ export default {
           }
         })
         .then(response => {
+          console.log(response);
           this.loadView(response.data);
         })
         .catch(error => {
+          console.log(error);
           switch (error.response.status) {
             case 401:
               axios
