2e8cee84e2b2458b94f3354485fe5cc283fda894
Ryan <43230399+rfoster93@users.noreply.github.com>
2021-04-30 21:42:21 +0200

    added name to error msg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -543,7 +543,7 @@ export default {
                 });
               break;
             default:
-              this.failedConfirm();
+              this.failedConfirm(shortName);
           }
         });
     },
